02ea7a4ecece7cb22ab4e1e133091685f6cf21fd
代川林 <2297362653@qq.com>
2025-02-01 17:19:30 +0800

    添加图片接口

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
+# from astrbot.api.all import *
 
-@register("helloworld", "Your Name", "一个简单的 Hello World 插件", "1.0.0")
-class MyPlugin(Star):
-    def __init__(self, context: Context):
+@register("Development", "daiHao4312", "一个简单的功能拓展插件", "1.0.0")
+class DevelopmentPlugin(Star):
+    def __init__(self, context: Context, config: dict):
+        self.config = config
         super().__init__(context)
     
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        user_name = event.get_sender_name()
-        yield event.plain_result(f"Hello, {user_name}!") # 发送一条纯文本消息
+    @filter.command("anime")  # 获取随机动漫图片
+    async def anime(self, event: AstrMessageEvent):
+        yield event.image_result(self.config.image_api)  # 发送图片
